a30f8eab9c7cc544389dcd1e54bdb75771f71068
markus <datamastery87@gmail.com>
2020-10-05 20:21:47 +0200

    basics implemented

--- Crashkurs/crashkurs.py
+++ Crashkurs/crashkurs.py
@@ -12,9 +12,27 @@ y = 3.2
 z = x + y
 type(z)
 
-# Sequence types (list, tuple, set, string, ) mutability
+# Sequence types (list, tuple, set, string) mutability
+einkaufsliste = ["apfel", "birne", "Milch"]
+einkaufsliste[0]
+einkaufsliste[0:2]
+einkaufsliste[-1]
+einkaufsliste[-1] = "H-Milch"
+einkaufsliste
+
+gemischte_liste = [1, 2, "test"]
+einkaufstupel = ("apfel", "birne", "Milch", "birne")
+einkaufstupel[1] = "Pfirsisch"
+
+einkaufsset = {"apfel", "birne", "Milch", "birne"}
+einkaufsset[1] = "Pfirsisch"
+string = "test"
+string[1] = "a"
 
 # Mapping types (dict)
+einkaufsdictionary = {"Milch": 0.99, "Apfel": 0.6}
+einkaufsdictionary["Milch"]
 
 # Booleans (bool)
-
+True
+False
