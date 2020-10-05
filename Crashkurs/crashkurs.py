0e0eb34cd0891fcbbabe6f13fee55d0186670347
markus <datamastery87@gmail.com>
2020-10-05 20:05:34 +0200

    basics

---
+++ Crashkurs/crashkurs.py
@@ -0,0 +1,20 @@
+### Datentypen ###
+
+# Strings (str)
+'einfache Anführungszeichen'
+"einfache Anführungszeichen"
+'Ich bin "nested"'
+
+string = "test"
+# Numeric (int, float)
+x = 3
+y = 3.2
+z = x + y
+type(z)
+
+# Sequence types (list, tuple, set, string, ) mutability
+
+# Mapping types (dict)
+
+# Booleans (bool)
+
