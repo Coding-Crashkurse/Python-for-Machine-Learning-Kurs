4bcc68dba434a60885385d1e1cbb8faade1760c8
markus <datamastery87@gmail.com>
2020-10-06 20:15:26 +0200

    operatoren

---
+++ Crashkurs/operatoren.py
@@ -0,0 +1,50 @@
+### Operatoren ###
+
+# Vergleichsoperatoren
+
+2 > 1
+
+2 < 1
+
+2 >= 2
+
+3 <= 2
+
+"a" == "b"
+
+"a" != "b"
+
+"a" > "b"
+
+True == True
+
+[] == []
+
+# Logische Operatoren
+
+2 > 1 and 3 > 2
+
+2 > 1 or 3 > 2
+
+# Membership Operatoren
+
+2 in [1,2,3]
+
+[1,2] in [1,2,3]
+[1,2] in [[1,2],3]
+
+1 not in [1,2,3]
+
+# Arithmetische Operatoren
+#2+2
+#2-2
+#3*3
+#3/2
+#3%2
+#3**3
+
+#3 + "abc"
+#3 * "abc"
+#"abcabcabc" / 3
+#3 * [1,2,3]
+#3 + [1,2,3]
