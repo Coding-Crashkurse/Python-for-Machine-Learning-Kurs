3c505b327806e50eefba0d066167e5e78d382513
markus <datamastery87@gmail.com>
2020-10-14 18:43:11 +0200

    updated course

---
+++ Crashkurs/objects.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 13 19:38:02 2020
+
+@author: User
+"""
+
+
+isinstance(1.2, object)
+
+# Objects haben Attribute und Methoden 
+
+"test".__len__()
+"test".capitalize()
+
+x = [1,2,3,4,5]
+result = []
+
+for zahl in x:
+    if zahl > 3:
+        zahl = zahl / 2
+        result.append(zahl)
+    else:
+        result.append(zahl)
+        
+print(result)
+    
