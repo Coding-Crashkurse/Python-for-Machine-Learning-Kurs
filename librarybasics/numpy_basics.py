28185aab64e9e52e559af7b8c3751090a88534e0
markus <datamastery87@gmail.com>
2020-11-07 20:11:32 +0100

    numpy arrays

--- librarybasics/numpy_basics.py
+++ librarybasics/numpy_basics.py
@@ -29,7 +29,68 @@ b = [1, 2, 3]
 
 a * b
 
-np_a = np.array([1, 2, 3])
-np_b = np.array([1, 2, 3])
+np_a = np.array([3, 4, 1])
+np_b = np.array([2, 3, 1])
 
 np_a * np_b
+
+## Numpy Grundlagen
+np.arange(4)
+np.zeros(4)
+np.ones(4)
+
+# Indexing
+arr = np.arange(11)
+arr > 2
+arr[arr > 2]
+
+arr[0:3]
+
+arr2 = arr
+arr[2] = 10
+arr2
+
+arr2 = arr.copy()
+
+# Operationen
+
+
+np.random.rand(10)
+
+np.random.seed(999)
+np.random.rand(10)
+
+np.random.seed(999)
+np.random.rand(10)
+
+np.sort(np_a)
+
+
+a = np.array([[1,2],[1,2]])
+b = np.array([[3,4]])
+b
+
+result = np.concatenate((a, b), axis = 0)
+result.shape
+
+result.reshape(2,3)
+
+
+result * 3
+result.sum()
+result.min()
+result.max()
+
+result.sum(axis=1)
+result.sum(axis=0)
+
+np.unique(result)
+
+result.transpose()
+
+np.flip(result)
+
+result.flatten()
+
+
+
